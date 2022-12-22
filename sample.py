aa69a97f89c059d93975c5672a34e073c04b7eb8
mokamoto-ri <mokamoto@ridge-i.com>
2022-12-22 13:58:48 +0900

    :tada: First commit

---
+++ sample.py
@@ -0,0 +1,23 @@
+# TODO: xxx
+print("TODO")
+
+# FIXME: xxx
+print("FIXME")
+
+# HACK: xxx
+print("HACK")
+
+# XXX: xxx
+print("XXX")
+
+# REVIEW: xxx
+print("REVIEW")
+
+# OPTIMIZE: xxx
+print("OPTIMIZE")
+
+# NOTE: xxx
+print("NOTE")
+
+# WARNING: xxx
+print("WARNING")
